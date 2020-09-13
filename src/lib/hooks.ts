0344d5c3cdf904b323837aa90f5c3d32bc2d796e
qimasho <qimasho@tutanota.com>
2020-09-13 17:11:16 +0200

    Type housekeeping

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -126,30 +126,6 @@ export function useElementSize(
 
 	return sizes;
 }
-/**
- * TS doesn't have a definition for it yet o.O. WTF it has 85% browser support.
- * Types from: https://gist.github.com/strothj/708afcf4f01dd04de8f49c92e88093c3
- */
-declare class ResizeObserver {
-	constructor(callback: ResizeObserverCallback);
-	disconnect: () => void;
-	observe: (target: Element, options?: ResizeObserverObserveOptions) => void;
-	unobserve: (target: Element) => void;
-}
-interface ResizeObserverObserveOptions {
-	box?: 'content-box' | 'border-box';
-}
-type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
-interface ResizeObserverEntry {
-	readonly borderBoxSize: ResizeObserverEntryBoxSize;
-	readonly contentBoxSize: ResizeObserverEntryBoxSize;
-	readonly contentRect: DOMRectReadOnly;
-	readonly target: Element;
-}
-interface ResizeObserverEntryBoxSize {
-	blockSize: number;
-	inlineSize: number;
-}
 
 /**
  * Checks how many items fit per row in grid styles element, and updates the
--- src/types.d.ts
+++ src/types.d.ts
@@ -8,3 +8,28 @@ declare function GM_openInTab(
 		incognito?: boolean; // makes the tab being opened inside a incognito mode/private mode window.
 	}
 ): void;
+
+/**
+ * TS doesn't have a definition for it yet o.O. WTF it has 85% browser support.
+ * Types from: https://gist.github.com/strothj/708afcf4f01dd04de8f49c92e88093c3
+ */
+declare class ResizeObserver {
+	constructor(callback: ResizeObserverCallback);
+	disconnect: () => void;
+	observe: (target: Element, options?: ResizeObserverObserveOptions) => void;
+	unobserve: (target: Element) => void;
+}
+interface ResizeObserverObserveOptions {
+	box?: 'content-box' | 'border-box';
+}
+type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
+interface ResizeObserverEntry {
+	readonly borderBoxSize: ResizeObserverEntryBoxSize;
+	readonly contentBoxSize: ResizeObserverEntryBoxSize;
+	readonly contentRect: DOMRectReadOnly;
+	readonly target: Element;
+}
+interface ResizeObserverEntryBoxSize {
+	blockSize: number;
+	inlineSize: number;
+}
