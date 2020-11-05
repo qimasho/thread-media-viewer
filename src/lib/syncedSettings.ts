982d0ffe28dabaa00cbf78dc35ab4cedfd5a5780
qimasho <qimasho@tutanota.com>
2020-11-05 18:08:16 +0100

    Store settings in grease monkey storage instead of localStorage

--- src/lib/syncedSettings.ts
+++ src/lib/syncedSettings.ts
@@ -14,18 +14,73 @@ export interface SyncedSettingsControls<T extends any> {
 
 export type SyncedSettings<T extends SettingsData> = T & SyncedSettingsControls<T>;
 
+export interface StorageDriver {
+	subscribersMap: Map<any, any>;
+	get<T extends unknown = unknown>(name: string): T | undefined;
+	set(name: string, value: unknown): void;
+	subscribe(name: string, callback: (name: string, oldValue: unknown, newValue: unknown) => void): () => void;
+	unsubscribe(id: any): void;
+}
+
+export const localStorageDriver: StorageDriver = {
+	subscribersMap: new Map(),
+	get: (name) => {
+		try {
+			return JSON.parse(localStorage.getItem(name) as any);
+		} catch {
+			return undefined;
+		}
+	},
+	set: (name, value) => localStorage.setItem(name, JSON.stringify(value)),
+	subscribe: (name, callback) => {
+		const listener = () => callback(name, undefined, localStorage.getItem(name));
+		localStorageDriver.subscribersMap.set(callback, listener);
+		window.addEventListener('storage', listener);
+		return () => localStorageDriver.unsubscribe(callback);
+	},
+	unsubscribe: (callback) => {
+		const listener = localStorageDriver.subscribersMap.get(callback);
+		if (listener) {
+			window.removeEventListener('storage', listener);
+			localStorageDriver.subscribersMap.delete(callback);
+		}
+	},
+};
+
+export const greaseMonkeyDriver: StorageDriver = {
+	subscribersMap: new Map(),
+	get: (name) => GM_getValue(name),
+	set: (name, value) => GM_setValue(name, value),
+	subscribe: (name, callback) => {
+		const id = GM_addValueChangeListener(name, callback);
+		greaseMonkeyDriver.subscribersMap.set(callback, id);
+		return () => greaseMonkeyDriver.unsubscribe(callback);
+	},
+	unsubscribe: (callback) => {
+		const id = localStorageDriver.subscribersMap.get(callback);
+		if (id) {
+			GM_removeValueChangeListener(id);
+			greaseMonkeyDriver.subscribersMap.delete(callback);
+		}
+	},
+};
+
 /**
  * localStorage wrapper that saves into a namespaced key as json, and provides
  * synchronization between tabs and change subscriptions.
  *
  * ```
- * let settings = syncedSettings('localStorageKey', {defaults: 'foo}); // pre-loads
+ * let settings = syncedSettings('storageKey', {defaults: 'foo}); // pre-loads
  * settings.foo; // retrieve
  * settings.foo = 5; // saves to localStorage automatically (debounced by 10ms)
  * const unsubscribe = settings._subscribe((settings) => {}); // called when this or other tab changes settings
  * ```
  */
-export function syncedSettings<T extends SettingsData>(localStorageKey: string, defaults: T): SyncedSettings<T> {
+export function syncedSettings<T extends SettingsData>(
+	storageKey: string,
+	defaults: T,
+	driver: StorageDriver = greaseMonkeyDriver
+): SyncedSettings<T> {
 	const listeners: Set<Callback<T>> = new Set();
 	let savingPromise: Promise<void> | null = null;
 	let settings: T = load();
@@ -35,21 +90,15 @@ export function syncedSettings<T extends SettingsData>(localStorageKey: string, 
 	}
 
 	function load(): T {
-		let json = localStorage.getItem(localStorageKey);
-		let data;
-		try {
-			data = json ? {...defaults, ...JSON.parse(json)} : {...defaults};
-		} catch (error) {
-			data = {...defaults};
-		}
-		return data;
+		let data = driver.get(storageKey);
+		return typeof data === 'object' ? {...defaults, ...data} : {...defaults};
 	}
 
 	function save() {
 		if (savingPromise) return savingPromise;
 		savingPromise = new Promise((resolve) =>
 			setTimeout(() => {
-				localStorage.setItem(localStorageKey, JSON.stringify(settings));
+				driver.set(storageKey, settings);
 				savingPromise = null;
 				resolve();
 			}, 10)
@@ -58,8 +107,8 @@ export function syncedSettings<T extends SettingsData>(localStorageKey: string, 
 	}
 
 	// Listen for changes from another tab
-	window.addEventListener(
-		'storage',
+	driver.subscribe(
+		storageKey,
 		throttle(() => {
 			let newData = load();
 			let hasChanges = false;
@@ -99,9 +148,7 @@ export function syncedSettings<T extends SettingsData>(localStorageKey: string, 
 		get(_, prop) {
 			if (isOfType<keyof typeof control>(prop, prop in control)) return control[prop];
 			if (isOfType<keyof T>(prop, prop in settings)) return settings[prop];
-			throw new Error(
-				`SyncedStorage: property "${String(prop)}" does not exist in "${localStorageKey}".`
-			);
+			throw new Error(`SyncedStorage: property "${String(prop)}" does not exist in "${storageKey}".`);
 		},
 		set(_, prop, value) {
 			if (isOfType<keyof T>(prop, prop in settings)) {
@@ -110,7 +157,7 @@ export function syncedSettings<T extends SettingsData>(localStorageKey: string, 
 				triggerListeners();
 				return true;
 			}
-			throw new Error(`Trying to set an unknown "${localStorageKey}" property "${String(prop)}"`);
+			throw new Error(`Trying to set an unknown "${storageKey}" property "${String(prop)}"`);
 		},
 	}) as unknown) as T;
 }
